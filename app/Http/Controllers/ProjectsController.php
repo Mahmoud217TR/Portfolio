d292e7aa2d0fff2d322bdcf2e2fdd2f1a995d490
Mahmoud217TR <faafet.mahmoud@gmail.com>
2021-09-22 00:14:10 +0300

    projects fixes

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class WorksController extends Controller
+class ProjectsController extends Controller
 {
     //
     public function index()
