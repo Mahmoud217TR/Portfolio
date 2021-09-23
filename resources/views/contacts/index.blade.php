6f510c0baa6d31fbfaaa4ab50dff4b4231e7e66c
Mahmoud217TR <faafet.mahmoud@gmail.com>
2021-09-23 09:02:14 +0300

    fixing messages

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col full-page d-flex justify-content-center align-items-center">
             <contact-component form-action = "{{ route('message.create') }}" contact-text ="Here is my contact info you can text me or call me when ever you want and I'll answer you as soon as posible.">
-                @csrf
+                <meta name="csrf-token" content="{{ csrf_token() }}">
             </contact-component>
         </div>
     </div>
